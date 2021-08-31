f4cb76092b70168f8f9e6c895b2f9749ad15bbcc
Andrew <twosecslater@snopyta.org>
2021-08-31 21:33:46 +0100

    initial commit

---
+++ src/gslash.cr
@@ -0,0 +1,33 @@
+require "db"
+require "pg"
+require "kemal"
+require "csv"
+require "yaml"
+
+config = YAML.parse(File.read("/etc/gslash.yml"))
+
+db = DB.open URI.new("postgres", host: config["db"]["host"].as_s,
+                     port: config["db"]["port"].as_i,
+                     user: config["db"]["user"].as_s,
+                     password: config["db"]["password"].as_s)
+
+post "/submit" do |env|
+  username = env.params.body["username"].as(String)
+  score = env.params.body["score"].to_u32
+  env.response.status_code = 200
+end
+
+get "/top" do |env|
+  env.response.headers["Content-Type"] = "text/csv"
+  result = CSV.build do |csv|
+    i = 1
+    while i <= 50
+      csv.row "Test Player #{i}", 4294967295_u32
+      i += 1
+    end
+  end
+  result
+end
+
+Kemal.config.powered_by_header = false
+Kemal.run
