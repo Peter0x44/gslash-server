6351b25c0b081686a09d6f88fe0b077e47093f8e
Andrew Pirie <twosecslater@snopyta.org>
2021-09-08 19:57:23 +0100

    fix offset

--- src/gslash.cr
+++ src/gslash.cr
@@ -73,7 +73,7 @@ get "/top" do |env|
     count = db.query_one("SELECT COUNT(*) FROM scores", as: {Int64}).to_s
     result = CSV.build do |csv|
       csv.row "count", count
-      db.query "SELECT players.uname, score FROM scores LEFT JOIN players ON scores.player = players.uid WHERE scores.uid >= (?) ORDER BY score DESC LIMIT 50", from.to_i32 do |row|
+      db.query "SELECT players.uname, score FROM scores LEFT JOIN players ON scores.player = players.uid ORDER BY score DESC LIMIT 50 OFFSET (?)", from.to_i32 do |row|
         row.each do
           csv.row row.read(String), row.read(Int64) # sqlite returns int64
         end
